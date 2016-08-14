22e21cf1fcd0fe0585559fd06677d1e4f0542865
TheDenys <box4den@gmail.com>
2016-08-14 18:24:34 +0200

    one more task solved

---
+++ NET4/NET4/Euler/P024_LexicographicPermutations.cs
@@ -0,0 +1,16 @@
+﻿using PDNUtils.Runner;
+using PDNUtils.Runner.Attributes;
+
+namespace NET4.Euler
+{
+    [RunableClass]
+    public class P024_LexicographicPermutations : RunableBase
+    {
+        [Run(0)]
+        public void SolveIt()
+        {
+            var kthPerm = Common.KthPerm(999999, 10);
+            DebugFormat("1 000 000 th perm of [0-9] is: {0}.", kthPerm);
+        }
+    }
+}
